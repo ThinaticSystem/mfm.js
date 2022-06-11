f09c778e21b44767a86d1eba13ab2998a8718afa
ThinaticSystem <git@thinaticsystem.com>
2022-06-11 16:00:38 +0900

    Revert "built/をコミット"
    This reverts commit 3af8221288065a655d83c3afdf43f7a8163a9756.

--- built/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { parse, parsePlain, toString, inspect, extract, } from './api';
-export { NodeType, MfmNode, MfmBlock, MfmInline, } from './node';
-export { MfmQuote, MfmSearch, MfmImageSearch, MfmCodeBlock, MfmMathBlock, MfmCenter, MfmUnicodeEmoji, MfmEmojiCode, MfmBold, MfmSmall, MfmSmell, MfmItalic, MfmStrike, MfmInlineCode, MfmMathInline, MfmMention, MfmHashtag, MfmUrl, MfmLink, MfmFn, MfmText, } from './node';
-export { QUOTE, SEARCH, IMAGE_SEARCH, CODE_BLOCK, MATH_BLOCK, CENTER, UNI_EMOJI, EMOJI_CODE, BOLD, SMALL, SMELL, ITALIC, STRIKE, INLINE_CODE, MATH_INLINE, MENTION, HASHTAG, N_URL, LINK, FN, TEXT, } from './node';
